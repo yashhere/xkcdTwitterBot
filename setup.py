7a0e02ca960ce04ab442f2b3ba3b9b54a6805e59
Yash Agarwal <yashagarwaljpr@gmail.com>
2017-06-08 17:56:15 +0530

    setup development environment

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+from setuptools import setup, find_packages
+import tweetgreet
+
+setup(name='TweetGreet',
+      version='1.0.0',
+      description='Automatically reply to Twitter users',
+      author='Yash Agarwal',
+      author_email='yashagarwaljpr@gmail.com',
+      packages=find_packages(),
+      url='https://www.github.com/yash2696/TweetGreet',
+      keywords=['GitHub', 'bot', 'Twitter', 'python'],
+      license='MIT',
+      # download_url='https://github.com/',
+      classifiers=[],
+      )
