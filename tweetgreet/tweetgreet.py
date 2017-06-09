e6f5853bcbe823f5705074a1d4e7ebebe7baee27
Yash Agarwal <yashagarwaljpr@gmail.com>
2017-06-09 09:59:37 +0530

    start work

--- tweetgreet/tweetgreet.py
+++ tweetgreet/tweetgreet.py
@@ -1,55 +1,21 @@
-from tweepy.streaming import StreamListener
-from tweepy import OAuthHandler, API
-from tweepy import Stream
-import json
-import logging
-import warnings
-from pprint import pprint
+import tweepy
+from secret import *
 
-warnings.filterwarnings("ignore")
+# create an OAuthHandler instance
+# Twitter requires all requests to use OAuth for authentication
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 
-access_token = "4464294380-GHJ3PY...lEqzZ8FikULPnkxaS4huT"
-access_token_secret = "L033NEsDOA...tZVoaU8prpFbiREWhcVROw2"
-consumer_key = "D1G0bn...9Y88p"
-consumer_secret = "rGJjCU...FRISsiMURYlqCXJvOP"
+auth.set_access_token(access_token, access_token_secret)
 
-auth_handler = OAuthHandler(consumer_key, consumer_secret)
-auth_handler.set_access_token(access_token, access_token_secret)
+# Construct the API instance
+api = tweepy.API(auth)  # create an API object
 
-twitter_client = API(auth_handler)
+# public_tweets = api.home_timeline()
+# for tweet in public_tweets:
+#     print(tweet.text)
 
-logging.getLogger("main").setLevel(logging.INFO)
-
-AVOID = ["monty", "leather", "skin", "bag", "blood", "bite"]
-
-
-class PyStreamListener(StreamListener):
-    def on_data(self, data):
-        tweet = json.loads(data)
-        try:
-            publish = True
-            for word in AVOID:
-                if word in tweet['text'].lower():
-                    logging.info("SKIPPED FOR {}".format(word))
-                    publish = False
-
-            if tweet.get('lang') and tweet.get('lang') != 'en':
-                publish = False
-
-            if publish:
-                twitter_client.retweet(tweet['id'])
-                logging.debug("RT: {}".format(tweet['text']))
-
-        except Exception as ex:
-            logging.error(ex)
-
-        return True
-
-    def on_error(self, status):
-        print(status)
-
-
-if __name__ == '__main__':
-    listener = PyStreamListener()
-    stream = Stream(auth_handler, listener)
-    stream.filter(track=['python', 'django', 'kivy', 'scrapy'])
+user = api.get_user('theyashagarwal')
+print(user.screen_name)
+print(user.followers_count)
+for friend in user.friends():
+    print(friend.screen_name)
